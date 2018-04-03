380c5904e2c8e5925c63c7c4f39504b04a7d1cf0
Kyle Serrecchia <kyserrecchia@gmail.com>
2018-04-03 15:02:31 -0700

    basic ui made and can grab user input

---
+++ public/index.js
@@ -0,0 +1,10 @@
+
+function getSubjectChoice(){
+	let selected = document.getElementById('subjects').value;
+	console.log(selected);
+}
+
+function getText(){
+	let textInput = document.getElementById('iaTextArea').value;
+	console.log(textInput);
+}
