2282500576618259d2eb8e7c0616294042abe890
Kyle Serrecchia <kyserrecchia@gmail.com>
2018-04-04 13:18:58 -0700

    string replacing of IAs working along with special styling!

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
+
 
 app.get('/', function (req, res) {
   res.sendFile('/index.html');
 });
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(PORT, function(){
 	console.log("running on 8080!");
 });
+
+// app.post('/posted', function(req,res){
+// 	const text = req.body.iaTextArea;
+// 	const pattern = req.body.subjects;
+// 	console.log(text);
+// 	console.log(pattern);
+// 	console.log("/////");
+// 	const index = ss.boyer_moore(text, pattern, true);
+// 	console.log(index);
+
+// 	res.redirect('/');
+// });
+
--- public/index.js
+++ public/index.js
@@ -1,10 +1,34 @@
 
-function getSubjectChoice(){
-	let selected = document.getElementById('subjects').value;
-	console.log(selected);
+let injectedDiv = document.getElementById('inject');
+
+function getTextAndSubjectChoice(){	
+	let selectedSubject = document.getElementById('subjects').value;
+	let IAs = getIAs(selectedSubject);
+	let textInput = document.getElementById('iaTextArea').value;
+
+	for(var i=0; i<IAs.length; i++){
+		let IA = IAs[i];
+		if(i===0){
+			let replaced = replacer(textInput, IA);
+			let injectedDiv = document.getElementById('inject');
+			injectedDiv.innerHTML = replaced;
+		} else{
+			let replaced = replacer(injectedDiv.innerHTML, IA);
+			injectedDiv.innerHTML = replaced;
+		}
+	}		
 }
 
-function getText(){
-	let textInput = document.getElementById('iaTextArea').value;
-	console.log(textInput);
+function getIAs(subject){
+	if(subject==="biology"){
+		return ["PCR", "RNA"];
+	}
+}
+
+function replacer(string, word){
+	// let regWord = "/" + word + "/g";
+	let spannedWord = "<span class='injectClass'>" + word + "</span>";
+	let newString = string.replace(new RegExp(word, 'g'),spannedWord);
+	// console.log(newString);
+	return newString; 
 }
