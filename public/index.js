d47b15de4346d4ebb30ac27268d23bbb9db28008
Kyle Serrecchia <kyserrecchia@gmail.com>
2018-04-04 15:14:27 -0700

    first working example!

--- public/index.js
+++ public/index.js
@@ -6,14 +6,15 @@ function getTextAndSubjectChoice(){
 	let IAs = getIAs(selectedSubject);
 	let textInput = document.getElementById('iaTextArea').value;
 
-	for(var i=0; i<IAs.length; i++){
+	for(var i=0; i<IAs.length; i+=2){
 		let IA = IAs[i];
+		let fullIA = IAs[i+1];
 		if(i===0){
-			let replaced = replacer(textInput, IA);
+			let replaced = replacer(textInput, IA, fullIA);
 			let injectedDiv = document.getElementById('inject');
 			injectedDiv.innerHTML = replaced;
 		} else{
-			let replaced = replacer(injectedDiv.innerHTML, IA);
+			let replaced = replacer(injectedDiv.innerHTML, IA, fullIA);
 			injectedDiv.innerHTML = replaced;
 		}
 	}		
@@ -21,13 +22,13 @@ function getTextAndSubjectChoice(){
 
 function getIAs(subject){
 	if(subject==="biology"){
-		return ["PCR", "RNA"];
+		return ["PCR", "Polymerase Chain Reaction", "RNA", "Ribonucleic Acid"];
 	}
 }
 
-function replacer(string, word){
+function replacer(string, word, fullWord){
 	// let regWord = "/" + word + "/g";
-	let spannedWord = "<span class='injectClass'>" + word + "</span>";
+	let spannedWord = "<span class='injectClass'>" + word + "<span class='tooltiptext'>" + fullWord + "</span></span>";
 	let newString = string.replace(new RegExp(word, 'g'),spannedWord);
 	// console.log(newString);
 	return newString; 
